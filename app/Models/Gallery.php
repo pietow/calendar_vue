5d634bcdc8efdff92367dd185e7564903fad837b
piet <pprotassoq@posteo.de>
2021-04-02 15:14:45 +0200

    many relationship

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\GalleryItem;
 
 class Gallery extends Model
 {
     use HasFactory;
+
+    public function galleryItems()
+    {
+        return $this->hasMany(GalleryItem::class);
+    }
 }
